f1336f9376b2c2cb764df3e46b29dc2f6b778b04
Rajesh-kumar-Roy <rajeshkumarroy01998@gmail.com>
2019-09-27 14:41:25 +0600

    initial commit

---
+++ RepoBranchTest/DbContextfor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RepoBranchTest.Models;
+
+namespace RepoBranchTest
+{
+    public class DbContextfor:DbContext
+    {
+        
+        public DbContextfor()
+            {
+
+            }
+
+            public DbContextfor(DbContextOptions options) : base(options)
+            {
+
+            }
+            public DbSet<Employee> Employees { get; set; }
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS ;Database = REPOTEST; Integrated Security=true;");
+            }
+        }
+}
